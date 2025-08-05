151b5c95da77d19d3bf5a196622b66fd302095d4
Saket Shubham <shubh12202003@gmail.com>
2025-08-05 12:28:34 +0000

    operations

---
+++ basics/4operations.js
@@ -0,0 +1,34 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue); 
+
+
+// console.log("\n")
+// console.log( 2+2); 
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " saket"
+let str3 = str1 + str2
+// console.log(str3)  
+
+// console.log("1" + 2); 
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "2");
+
+// console.log("true")
+// console.log(true)
+// console.log(+true); 
+// console.log("");
+// console.log(typeof "");
+// console.log(+""); 
+let gameScore = 100
+// gameScore++
+// console.log(gameScore); 
+++gameScore
+console.log(gameScore);
