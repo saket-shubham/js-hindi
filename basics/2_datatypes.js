59c0af2b5d01fc994c34b1308c7d8038e1b3fc0a
Saket Shubham <shubh12202003@gmail.com>
2025-08-05 08:34:25 +0000

    datatypes

---
+++ basics/2_datatypes.js
@@ -0,0 +1,26 @@
+ "use strict"; // treat all js code as new version
+
+
+// alert(3 + 3) // we are using nodejs, not browser
+ console.log(3 + 3);
+ 
+ let name = "saket"
+ let age = 25
+ let isLoggedIn = true
+
+// primitive data types
+
+//  Number ==> 2 to the power 53
+//  BigInt
+//  String ==> ""
+// Boolean ==> true/false
+//  null ==> empty value or standalone value
+let state = null
+//  undefined ==> variable is declared but not assigned any value
+// symbol ==> unique value
+
+
+//  objects
+
+ console.log(typeof null)  // object
+console.log(typeof undefined)   // undefined
